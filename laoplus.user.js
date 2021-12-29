18fb96ba26db23de58c74ed71a79151ef79ad131
darkautism <darkautism@users.noreply.github.com>
2021-12-29 08:04:36 +0000

    update with 2fbf56fc187890d077f990acc3bc04ec71b8a0a5

--- laoplus.user.js
+++ laoplus.user.js
@@ -689,30 +689,47 @@
                 React.createElement(Timer, { targetDate: enterDate, className: "pt-[50%] rotate-90" }))));
     };
 
+    GM_addStyle(`
+.icon {
+  height: 16px;
+  width: 16px;
+  display: inline-block;
+  vertical-align: middle;
+}
+`);
+
+    function jsonEqual(a, b) {
+        return JSON.stringify(a) === JSON.stringify(b);
+    }
     const ResourceFarmer = () => {
         const status = unsafeWindow.LAOPLUS.status;
-        const [stat, setStat] = React.useState(status.status.resourceFarmRecoder);
+        const [stat, setStat] = React.useState({ ...status.status.resourceFarmRecoder });
         status.events.on("changed", (e) => {
-            setStat(() => ({ ...e.resourceFarmRecoder }));
+            setStat((old) => {
+                if (!jsonEqual(old, e.resourceFarmRecoder))
+                    return { ...e.resourceFarmRecoder };
+                return old;
+            });
         });
         const style = {
-            textShadow: "black 0.1em 0.1em 0.2em"
+            textShadow: "black 0.1em 0.1em 0.2em",
         };
-        return (React.createElement("div", { className: "absolute top-0 left-0 w-1/2 px-3 ml-[5%]" },
-            React.createElement("div", { className: "text-white text-sm whitespace-nowrap", style: style },
-                "[",
-                stat.rounds,
-                "] ",
+        return (React.createElement("div", { className: "ml-[5%] absolute left-0 top-0 px-3 w-1/2" },
+            React.createElement("div", { className: "text-slate-200 whitespace-nowrap text-sm font-semibold", style: style },
+                "[Round:",
+                React.createElement("div", { className: "text-emerald-300 inline-block" }, stat.rounds),
+                "]",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/a/ab/Part_Icon.png" }),
                 stat.Metal,
-                "/",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/f/f4/Nutrient_Icon.png" }),
                 stat.Nutrient,
-                "/",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/d/d0/Power_Icon.png" }),
                 stat.Power,
-                " - ",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/2/2e/Basic_Module_Icon.png" }),
                 stat.Normal_Module,
-                "/",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/d/d3/Advanced_Module_Icon.png" }),
                 stat.Advanced_Module,
-                "/",
+                React.createElement("img", { className: "icon", src: "https://static.wikia.nocookie.net/lastorigin/images/9/9c/Special_Module_Icon.png" }),
                 stat.Special_Module)));
     };
 
